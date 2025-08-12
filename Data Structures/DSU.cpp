4049b450648990ec59a9933fe630fe3bddc4f7d6
Abdullah Barakat <86885465+drynor@users.noreply.github.com>
2025-08-12 21:20:01 +0300

    Create DSU.cpp

---
+++ Data Structures/DSU.cpp
@@ -0,0 +1,74 @@
+struct DSU {
+    vector<int> p, sz;
+    int n;
+ 
+    DSU(int _n) : n(_n) {
+        p.resize(n);
+        sz.resize(n, 1);
+        iota(p.begin(), p.end(), 0);
+    }
+ 
+    inline int get(int x) {
+        return (x == p[x] ? x : (p[x] = get(p[x])));
+    }
+ 
+    inline bool unite(int x, int y) {
+        x = get(x);
+        y = get(y);
+        if (x != y) {
+            if (sz[x] < sz[y]) {
+                swap(x, y); 
+            }
+            p[y] = x;
+            sz[x] += sz[y];
+            return true;
+        }
+        return false;
+    }
+ 
+    inline int size(int x) {
+        return sz[get(x)];
+    }
+};
+
+// DSU WITH ROLLBACK
+struct DSU {
+    vector<int> p, sz;
+    stack<tuple<int, int, int, int>> history;
+    int components;
+
+    DSU(int n){
+        p.resize(n + 1);
+        iota(p.begin(), p.end(), 0);
+        sz.assign(n + 1, 1);
+        components = n;
+    }
+
+    int get(int x){
+        return x == p[x] ? x : get(p[x]);
+    }
+
+    void unite(int x, int y){
+        x = get(x);
+        y = get(y);
+        if (x != y) {
+            if (sz[x] < sz[y]){
+                swap(x, y);
+            }
+            history.push({x, sz[x], y, p[y]});
+
+            p[y] = x;
+            sz[x] += sz[y];
+            components--;
+        }
+    }
+
+    void rollback(){
+        auto [root_x, old_sz_x, root_y, old_p_y] = history.top();
+        history.pop();
+        
+        p[root_y] = old_p_y;
+        sz[root_x] = old_sz_x;
+        components++;
+    }
+};
