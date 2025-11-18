bcdb46a73ab6dfcaa074eda12ef9a2ddc59c2784
Abdullah Barakat <86885465+drynor@users.noreply.github.com>
2025-11-18 17:14:04 +0300

    Update SparseTable.cpp

--- Data Structures/SparseTable.cpp
+++ Data Structures/SparseTable.cpp
@@ -1,3 +1,18 @@
+struct ST {
+    vector<vector<int>> st;
+    ST(vector<int>& a) {
+        int n = a.size(), k = __lg(n);
+        st.assign(k + 1, a);
+        for (int j = 0; j < k; j++)
+            for (int i = 0; i + (2 << j) <= n; i++)
+                st[j + 1][i] = min(st[j][i], st[j][i + (1 << j)]);
+    }
+    int query(int l, int r) {
+        int k = __lg(r - l + 1);
+        return min(st[k][l], st[k][r - (1 << k) + 1]);
+    }
+};
+
 struct min_st{
     int n;
     vector<int> lg;
