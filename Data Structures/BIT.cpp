6471796a53cd16607bd6f45dbe0b2dfcf50e71e2
Abdullah Barakat <86885465+drynor@users.noreply.github.com>
2025-11-20 17:37:01 +0300

    Clean up comments in BIT.cpp
    Removed comments for the 0-Indexed RUPQ Fenwick Tree and retained the 0-Indexed PURQ Fenwick Tree documentation.

--- Data Structures/BIT.cpp
+++ Data Structures/BIT.cpp
@@ -1,9 +1,3 @@
-/**
- * @brief 0-Indexed (Interface) / 1-Indexed (Storage) PURQ Fenwick Tree.
- * * This is the 0-indexed version of BIT2, using 1-indexed storage.
- * - update(idx, val): Adds val to the element at 0-indexed position idx.
- * - query(l, r): Returns the sum of the 0-indexed range [l, r].
- */
 template <class T> struct BIT {
     int n;
     vector<T> bit;
@@ -23,12 +17,7 @@ template <class T> struct BIT {
         for (int i = j + 1; i < n; i += (i & -i)) bit[i] += v;
     }
 };
-/**
- * @brief 0-Indexed (Interface) / 1-Indexed (Storage) RUPQ Fenwick Tree.
- * * This is the 0-indexed version of BIT1, using 1-indexed storage.
- * - update(l, r, val): Adds val to all elements in the 0-indexed range [l, r].
- * - query(idx): Returns the value of the element at 0-indexed position idx.
- */
+
 template <class T> struct BIT {
     int n;
     vector<T> bit;
