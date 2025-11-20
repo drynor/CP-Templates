886e7ee95eb3bc4d091dd6401406b11714422bed
Abdullah Barakat <86885465+drynor@users.noreply.github.com>
2025-11-20 18:01:09 +0300

    Refactor query handling and add functions

--- Data Structures/Mo's.cpp
+++ Data Structures/Mo's.cpp
@@ -4,29 +4,24 @@ struct Query {
 
 int B = max(1, int(sqrt(n)));
 vector<Query> qs(q);
-  for (int i = 0; i < q; ++i) {
-      cin >> qs[i].l >> qs[i].r;
-      qs[i].idx = i;
-  }
-  sort(qs.begin(), qs.end(), [&](const Query &A, const Query &BQ){
-      int b1 = A.l / B, b2 = BQ.l / B;
-      if (b1 != b2) return b1 < b2;
-      return (b1 & 1) ? (A.r > BQ.r) : (A.r < BQ.r);
-  });
+for (int i = 0; i < q; ++i) {
+    cin >> qs[i].l >> qs[i].r;
+    qs[i].idx = i;
+}
 
-  vector<int> ans(q);
+sort(qs.begin(), qs.end(), [&](const Query &A, const Query &BQ){
+    int b1 = A.l / B, b2 = BQ.l / B;
+    if (b1 != b2) return b1 < b2;
+    return (b1 & 1) ? (A.r > BQ.r) : (A.r < BQ.r);
+});
 
-  int curL = 1, curR = 0;
-  for (auto &Q : qs) {
-      int L = Q.l, R = Q.r;
-      while (curL > L) add(a[--curL]);
-      while (curR < R) add(a[++curR]);
-      while (curL < L) remove_(a[curL++]);
-      while (curR > R) remove_(a[curR--]);
-      ans[Q.idx] = curAns;
-  }
-
-  for(int i = 0;i<q;i++){
-      cout << ans[i] << " ";
-  }
-  cout << endl;
+vector<int> ans(q);
+int curL = 1, curR = 0;
+for (auto &Q : qs) {
+    int L = Q.l, R = Q.r;
+    while (curL > L) addl(a[--curL]);
+    while (curR < R) addr(a[++curR]);
+    while (curL < L) rmvl(a[curL++]);
+    while (curR > R) rmvr(a[curR--]);
+    ans[Q.idx] = curAns;
+}
