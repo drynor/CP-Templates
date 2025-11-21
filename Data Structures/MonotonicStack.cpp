7676f4c35703c5f8d73818f69a6590dd6d41f435
Abdullah Barakat <86885465+drynor@users.noreply.github.com>
2025-11-21 15:54:35 +0300

    Simplify insert method in MonotonicStack
    Refactor insert method to reduce lines of code.

--- Data Structures/MonotonicStack.cpp
+++ Data Structures/MonotonicStack.cpp
@@ -1,10 +1,7 @@
 struct MonotonicStack{
     stack<pair<int,int>> st;
-    
     int insert(int ind, int val){
-        while(!st.empty() and st.top().s >= val){
-            st.pop();
-        }
+        while(!st.empty() and st.top().s >= val) st.pop();
         int ans;
         if(st.empty()) ans = -1;
         else           ans = st.top().f;
