4e621eb1302c7ceb0c27b14ee1be5ff4bb52f575
Abdullah Barakat <86885465+drynor@users.noreply.github.com>
2025-12-07 19:38:26 +0300

    Refactor DynamicSegmentTree structure and methods

--- Data Structures/Segment Trees/DynamicSegmentTree.cpp
+++ Data Structures/Segment Trees/DynamicSegmentTree.cpp
@@ -1,39 +1,37 @@
-struct Node {
-    int value;
-    Node *l;
-    Node *r;
-
-    Node() : value(0), l(nullptr), r(nullptr) {}
+struct node {
+    int val;
+    node *l, *r;
+    node() : val(0), l(nullptr), r(nullptr) {}
 };
-struct DST {
-    DST(int tmn, int tmx) : root(new Node()), mn(tmn), mx(tmx) {}
+struct seg {
+    seg(int tmn, int tmx) : root(new node()), mn(tmn), mx(tmx) {}
 
-    Node* root;
+    node* root;
     int mn, mx;
 
     void upd(int ind, int v) {
         upd(root, mn, mx, ind, v);
     }
-    void upd(Node* cn, int nl, int nr, int i, int v) {
-        cn->value += v;
+    void upd(node* cn, int nl, int nr, int i, int v) {
+        cn->val += v;
         if (nl == nr) return;
 
         int m = nl + (nr - nl) / 2;
         if (i <= m){
-            if(!cn->l) cn->l = new Node();
+            if(!cn->l) cn->l = new node();
             upd(cn->l, nl, m, i, v);
         }
         else{
-            if(!cn->r) cn->r = new Node();
+            if(!cn->r) cn->r = new node();
             upd(cn->r, m + 1, nr, i, v);
         }
     }
     int qry(int ql, int qr) {
         return qry(root, mn, mx, ql, qr);
     }
-    int qry(Node* cn, int nl, int nr, int ql, int qr) {
+    int qry(node* cn, int nl, int nr, int ql, int qr) {
         if (!cn || nr < ql || nl > qr) return 0;
-        if(nl >= ql && nr <= qr) return cn->value;
+        if(nl >= ql && nr <= qr) return cn->val;
         int m = nl + (nr - nl) / 2;
         return qry(cn->l, nl, m, ql, qr) + qry(cn->r, m + 1, nr, ql, qr);
     }
