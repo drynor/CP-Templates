03a0f7d82ab5042646cfaf5315bde072ef2f2317
Abdullah Barakat <86885465+drynor@users.noreply.github.com>
2026-03-04 19:34:13 +0300

    Create FFT.cpp

---
+++ Math/FFT.cpp
@@ -0,0 +1,42 @@
+using cd = complex<double>;
+const double PI = acos(-1);
+
+void fft(vector<cd> & a, bool invert) {
+    int n = a.size();
+
+    for (int i = 1, j = 0; i < n; i++) {
+        int bit = n >> 1;
+        for (; j & bit; bit >>= 1) j ^= bit;
+        j ^= bit;
+        if (i < j) swap(a[i], a[j]);
+    }
+
+    for(int len = 2; len <= n; len <<= 1){
+        cd omg = exp(cd(0, 2.0 * PI / len * (invert ? -1 : 1)));
+        for(int i = 0; i < n; i += len) {
+            auto curOmg = cd(1, 0);
+            for(int j = 0; j < len/2; ++j) {
+                cd e = a[i + j], o = a[i + j + len/2] * curOmg;
+                a[i + j] = e + o;
+                a[i + j + len/2] = e - o;
+                curOmg *= omg;
+            }
+        }
+    }
+}
+
+vector<int> mul(vector<int> & a, vector<int> & b) {
+    vector<cd> fa(a.begin(), a.end()), fb(b.begin(), b.end());
+    int n = 1;
+    while (n < a.size() + b.size()) n <<= 1;
+    fa.resize(n), fb.resize(n);
+
+    fft(fa, false), fft(fb, false);
+    for (int i = 0; i < n; i++) fa[i] *= fb[i];
+    fft(fa, true);
+
+    vector<int> result(n);
+    for (int i = 0; i < n; i++)
+        result[i] = round(fa[i].real() / n);
+    return result;
+}
