acffea018374f062c5395e61d9218e82e2d117e4
Abdullah Barakat <86885465+drynor@users.noreply.github.com>
2026-04-15 21:17:03 +0300

    Update KMP.cpp

--- String Algo/KMP.cpp
+++ String Algo/KMP.cpp
@@ -1,21 +1,21 @@
-vector<int> build_fail(string a) {
+vector<int> fail(string a) {
     int n = a.size(), ln = 0;
-    vector<int> fail(n, 0);
+    vector<int> f(n, 0);
     for(int i = 1; i < n; i++){
-        while(ln > 0 and a[ln] != a[i]) ln = fail[ln-1];
+        while(ln > 0 and a[ln] != a[i]) ln = f[ln-1];
         if(a[ln] == a[i]) ln++;
-        fail[i] = ln;
+        f[i] = ln;
     }
-    return fail;
+    return f;
 }
-void kmp(string a, string b, vector<int> fail){
+void kmp(string a, string b, vector<int> f){
     int n = a.size(), m = b.size(), ln = 0;
     for(int i = 0;i<n;i++){
-        while(ln > 0 and b[ln] != a[i]) ln = fail[ln-1];
+        while(ln > 0 and b[ln] != a[i]) ln = f[ln-1];
         if(b[ln] == a[i]) ln++;
         if(ln == m){
             cout << i - ln + 1 << endl;
-            ln = fail[ln-1];
+            ln = f[ln-1];
         }
     }
 }
