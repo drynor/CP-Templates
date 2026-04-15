393c363af6d09c17ac979bedf9dbc77fb3a5d110
Abdullah Barakat <86885465+drynor@users.noreply.github.com>
2026-04-15 21:27:44 +0300

    Refactor suffix automaton structure and constructor

--- String Algo/suffixAutomaton.cpp
+++ String Algo/suffixAutomaton.cpp
@@ -1,23 +1,19 @@
-struct state {
-    int len, link;
-    map<char, int> next;
-};
-
 struct sam {
+    struct state {
+        int len, link;
+        map<char, int> next;
+    };
+    
     vector<state> st;
     int sz, last;
-    sam(const string &s) {
-        st.resize(s.length() * 2);
-        st[0].len = 0; st[0].link = -1;
-        sz = 1; last = 0;
-        for(char c : s){
-            extend(c);
-        }
+    sam(string &s) : st(s.size() * 2), sz(1), last(0) {
+        st[0] = {0, -1, {}};
+        for(char c : s) extend(c);
     }
     void extend(char c){
-        int cur = sz++;
+        int cur = sz++, p = last;
         st[cur].len = st[last].len + 1;
-        int p = last;
+
         while (p != -1 && !st[p].next.count(c)) {
             st[p].next[c] = cur;
             p = st[p].link;
@@ -30,9 +26,7 @@ struct sam {
                 st[cur].link = q;
             } else {
                 int nw = sz++;
-                st[nw].len = st[p].len + 1;
-                st[nw].next = st[q].next;
-                st[nw].link = st[q].link;
+                st[nw] = {st[p].len+1, st[q].link, st[q].next};
                 while (p != -1 && st[p].next[c] == q) {
                     st[p].next[c] = nw;
                     p = st[p].link;
