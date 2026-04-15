8dc952b156cd265003a863154040996015390d1d
Abdullah Barakat <86885465+drynor@users.noreply.github.com>
2026-04-15 22:01:27 +0300

    Update AhoCorasick.cpp

--- String Algo/AhoCorasick.cpp
+++ String Algo/AhoCorasick.cpp
@@ -2,43 +2,51 @@ struct AC {
     int N;
     vector<vector<int>> next, g;
     vector<int> link;
-    // vector<string> str;
-    AC() {
-        N = 0; node();
-    }
+    // vector<int> out_link, out; vector<string> str;
+    
+    AC() { N = 0; node(); }
+    
     int node() {
         next.emplace_back(26, 0);
         link.emplace_back(0);
+
+        // out.emplace_back(-1);
+        // out_link.emplace_back(0);
         // str.emplace_back("");
         return N++;
     }
-    void add(string s) {
+    
+    void add(string s, int id = 1) { 
         int u = 0;
         for (char c : s) {
             int i = c - 'a';
             if (!next[u][i]) {
-                int v = node();
-                // str[v] = str[u] + c;
-                next[u][i] = v;
+                next[u][i] = node(); 
+                // int v = node(); str[v] = str[u] + c; next[u][i] = v;
             }
             u = next[u][i];
-        }
-        return;
+        } 
+        // out[u] = id;
     }
+    
     void compute() {
         queue<int> q; q.push(0);
         while (!q.empty()) {
             int u = q.front(); q.pop();     
             for (int c = 0; c < 26; c++) {
                 int v = next[u][c];
-                if (!v) next[u][c] = next[link[u]][c];
-                else {
-                    link[v] = (u ? next[link[u]][c] : 0);                    
+                if (!v) {
+                    next[u][c] = next[link[u]][c];
+                } else {
+                    link[v] = (u ? next[link[u]][c] : 0);      
+                    //out_link[v] = (~out[link[v]]) ? link[v] : out_link[link[v]];     
                     q.push(v);
                 }
             }
-        } // g.resize(N); // for (int i = 1; i < N; i++) g[link[i]].push_back(i);
+        } 
+        // g.resize(N); for (int i = 1; i < N; i++) g[link[i]].push_back(i);
     }
+    
     int adv(int u, char c) {
         return next[u][c - 'a'];
     }
