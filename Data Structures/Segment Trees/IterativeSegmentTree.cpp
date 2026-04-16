dc5c3442c4547ffdf25b0c6c45d6a495738eb2ef
Abdullah Barakat <86885465+drynor@users.noreply.github.com>
2026-04-17 00:18:04 +0300

    Update IterativeSegmentTree.cpp

--- Data Structures/Segment Trees/IterativeSegmentTree.cpp
+++ Data Structures/Segment Trees/IterativeSegmentTree.cpp
@@ -1,24 +1,34 @@
-const int N = 1e6;
-int n;
-int tree[N * 2];
+struct sgt {
+    int n;
+    vector<int> t;
 
-void build() {
-    for(int i = n - 1; i > 0; i--) {
-        tree[i] = max(tree[i<<1], tree[i<<1|1]);
+    sgt(int tn, vector<int> &a) {
+        n = tn;
+        t.assign(2 * n, 0); 
+        for (int i = 0; i < n; i++) {
+            t[n + i] = a[i];
+        }
+        build();
     }
-}
 
-void upd(int i, int v) {
-    for (tree[i += n] = v; i > 1; i >>= 1) {
-        tree[i>>1] = max(tree[i], tree[i^1]);
+    void build() {
+        for(int i = n - 1; i > 0; i--) {
+            t[i] = max(t[i<<1], t[i<<1|1]);
+        }
     }
-}
 
-int qry(int l, int r) {
-    int ans = -1e9;
-    for (l += n, r += n; l <= r; l >>= 1, r >>= 1) {
-        if (l & 1)    ans = max(res, tree[l++]);
-        if (!(r & 1)) ans = max(res, tree[r--]);
+    void upd(int i, int v) {
+        for (t[i += n] = v; i > 1; i >>= 1) {
+            t[i>>1] = max(t[i], t[i^1]);
+        }
     }
-    return ans;
-}
+
+    int qry(int l, int r) {
+        int v = -1e9;
+        for (l += n, r += n; l <= r; l >>= 1, r >>= 1) {
+            if (l & 1)    v = max(v, t[l++]);
+            if (!(r & 1)) v = max(v, t[r--]);
+        }
+        return v;
+    }
+};
