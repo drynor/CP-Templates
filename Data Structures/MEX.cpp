979c4a8a672e685a3c165a41a1709a5e8bef723f
Abdullah Barakat <86885465+drynor@users.noreply.github.com>
2026-04-28 00:44:47 +0300

    Refactor MEX class for improved readability

--- Data Structures/MEX.cpp
+++ Data Structures/MEX.cpp
@@ -1,29 +1,16 @@
 const int N = 1e5 + 5;
 class MEX {
-    bitset<N> mex;
-    vector<int> f;
-    MEX() : f(N, 0){
-        mex.set();
-    }
+    bitset<N> mex; vector<int> f;
+    MEX() : f(N, 0){ mex.set(); }
     void add(int x){
-        if (x >= N) {
-            return;
-        }
+        if (x >= N) return;
         f[x]++;
-        if (f[x] == 1) {
-            mex.reset(x);
-        }
+        if (f[x] == 1) mex.reset(x);
     }
     void rmv(int x){
-        if (x >= N) {
-            return;
-        }
+        if (x >= N) return;
         f[x]--;
-        if (f[x] == 0) {
-            mex.set(x);
-        }
+        if (f[x] == 0) mex.set(x);
     }
-    int qry(){
-        return mex._Find_first();
-    }
+    int qry(){return mex._Find_first();}
 };
