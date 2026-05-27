90fd0fce0972119fa0c8676ef2d3f032d37178fb
Abdullah Barakat <86885465+drynor@users.noreply.github.com>
2026-05-27 11:46:28 +0300

    Add RollbackDSU implementation
    Implemented a rollback mechanism for the disjoint set union (DSU) structure, allowing for the reversal of union operations.

---
+++ Data Structures/DSU/RollBackDSU.cpp
@@ -0,0 +1,41 @@
+// dsu WITH ROLLBACK
+struct dsu {
+    vector<int> p, sz;
+    stack<tuple<int, int, int, int>> history;
+    int components;
+
+    dsu(int n){
+        p.resize(n + 1);
+        iota(p.begin(), p.end(), 0);
+        sz.assign(n + 1, 1);
+        components = n;
+    }
+
+    int get(int x){
+        return x == p[x] ? x : get(p[x]);
+    }
+
+    void unite(int x, int y){
+        x = get(x);
+        y = get(y);
+        if (x != y) {
+            if (sz[x] < sz[y]){
+                swap(x, y);
+            }
+            history.push({x, sz[x], y, p[y]});
+
+            p[y] = x;
+            sz[x] += sz[y];
+            components--;
+        }
+    }
+
+    void rollback(){
+        auto [root_x, old_sz_x, root_y, old_p_y] = history.top();
+        history.pop();
+        
+        p[root_y] = old_p_y;
+        sz[root_x] = old_sz_x;
+        components++;
+    }
+};
