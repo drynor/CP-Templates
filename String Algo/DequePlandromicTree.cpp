66bab4e8888669b74bbcc64d8ac23a77fddbdbd9
Abdullah Barakat <86885465+drynor@users.noreply.github.com>
2026-06-04 11:41:55 +0300

    Refactor deque_plandromic_query structure and methods

--- String Algo/DequePlandromicTree.cpp
+++ String Algo/DequePlandromicTree.cpp
@@ -1,177 +1,122 @@
-template<int alpha>
-struct deque_plandromic_query {
-    struct node {
-        array<node *, alpha> next;
-        array<node *, alpha> dlink;
-        node *link, *prev;
-        int len, linkcnt, cnt;
- 
-        node() : next{}, dlink{}, prev(nullptr), link(nullptr), len(0), linkcnt(0), cnt(0) {}
- 
-        void set_link(node *to) {
-            if (link) link->linkcnt -= 1;
-            link = to;
-            if (link) link->linkcnt += 1;
+struct eertree {
+    eertree(int q) {
+        q += 2;
+        cnt = len = par = link = slink = vector(q, 0);
+        to.resize(q);
+        link[0] = slink[0] = 1;
+        len[1] = -1;
+    }
+
+    template<bool back = 1>
+    static int get(auto const& d, int idx) {
+        if(idx >= size(d)) return -1;
+        else if constexpr (back) return prev(end(d))[-idx];
+        else return begin(d)[idx];
+    }
+    template<bool back = 1>
+    static void push(auto &d, auto c) {
+        if constexpr (back) d.push_back(c);
+        else d.push_front(c);
+    }
+    template<bool back = 1>
+    static void pop(auto &d) {
+        if constexpr (back) d.pop_back();
+        else d.pop_front();
+    }
+
+    template<bool back = 1>
+    void add_letter(char c) {
+        c -= 'a';
+        push<back>(s, c);
+        int pre = get<back>(states, 0);
+        int last = make_to<back>(pre, c);
+        active += !(cnt[last]++);
+        int D = 2 + len[pre] - len[last];
+        while(D + len[pre] <= len[last]) {
+            pop<back>(states);
+            if(!empty(states)) {
+                pre = get<back>(states, 0);
+                D += get<back>(diffs, 0);
+                pop<back>(diffs);
+            }
+            else break;
         }
-    };
- 
-    int node_cnt;
- 
-    node *new_node(int len) {
-        node_cnt += 1;
-        node *it = new node();
-        it->len = len;
-        return it;
+        if(!empty(states)) push<back>(diffs, D);
+        push<back>(states, last);
     }
- 
-    vector<node *> to_del;
- 
-    void del_node(node *v) {
-        node_cnt -= 1;
-        v->link->linkcnt -= 1;
-        to_del.push_back(v);
-    }
- 
-    int start_pos, end_pos;
-    node *even, *odd;
-    deque<int> data;
-    deque<node *> presurf, sufsurf;
- 
-    auto &get_data(int pos) {
-        assert(start_pos <= pos && pos < end_pos);
-        return data[pos - start_pos];
-    }
- 
-    auto &get_presurf(int pos) {
-        assert(start_pos <= pos && pos < end_pos);
-        return presurf[pos - start_pos];
-    }
- 
-    auto &get_sufsurf(int pos) {
-        assert(start_pos <= pos && pos < end_pos);
-        return sufsurf[pos - start_pos];
-    }
- 
-    bool unique(node *v) {
-        return v->linkcnt == 0 && v->cnt == 1;
-    }
- 
-    node *get_prepal() {
-        return !presurf.empty() ? presurf.front() : even;
-    }
- 
-    node *get_sufpal() {
-        return !sufsurf.empty() ? sufsurf.back() : even;
-    }
- 
-    deque_plandromic_query() {
-        node_cnt = 0;
-        start_pos = end_pos = 0;
-        odd = new_node(-1);
-        odd->set_link(odd);
-        even = new_node(0);
-        even->set_link(odd);
-        for (int x = 0; x < alpha; ++x)
-            odd->dlink[x] = even->dlink[x] = odd;
-    }
- 
-    void push_back(int x) {
-        node *v = get_sufpal();
-        data.push_back(x);
-        presurf.push_back(even);
-        sufsurf.push_back(even);
-        end_pos += 1;
-        auto get_suf_link = [&](node *v, int x) -> node * {
-            return end_pos - 1 - v->len - 1 >= start_pos && get_data(end_pos - 1 - v->len - 1) == x ? v : v->dlink[x];
-        };
-        node *sufpal = get_suf_link(v, x);
-        if (!sufpal->next[x]) {
-            node *tmp = new_node(sufpal->len + 2);
-            tmp->prev = sufpal;
-            node *tmp_link = get_suf_link(sufpal->link, x)->next[x];
-            if (!tmp_link) tmp_link = even;
-            tmp->set_link(tmp_link);
-            tmp->dlink = tmp->link->dlink;
-            tmp->dlink[get_data(end_pos - 1 - tmp->link->len)] = tmp->link;
-            sufpal->next[x] = tmp;
+    template<bool back = 1>
+    void pop_letter() {
+        int last = get<back>(states, 0);
+        active -= !(--cnt[last]);
+        pop<back>(states);
+        pop<back>(s);
+        array cands = {pair{link[last], len[last] - len[link[last]]},
+                       pair{par[last], 0}};
+        for(auto [state, diff]: cands) {
+            if(empty(states)) {
+                states = {state};
+                diffs = {diff};
+            } else {
+                int D = get<back>(diffs, 0) - diff;
+                int pre = get<back>(states, 0);
+                if(D + len[state] > len[pre]) {
+                    push<back>(states, state);
+                    pop<back>(diffs);
+                    push<back>(diffs, D);
+                    push<back>(diffs, diff);
+                }
+            }
         }
-        node *vp = sufpal->next[x];
-        get_presurf(end_pos - 1) = even;
-        get_presurf(end_pos - vp->len) = vp;
-        get_sufsurf(end_pos - 1) = vp;
-        if (vp->link->len >= 1 && get_sufsurf(end_pos - vp->len + vp->link->len - 1) == vp->link)
-            get_sufsurf(end_pos - vp->len + vp->link->len - 1) = even;
-        vp->cnt += 1;
+        pop<back>(diffs);
     }
- 
-    void push_front(int x) {
-        node *v = get_prepal();
-        data.push_front(x);
-        presurf.push_front(even);
-        sufsurf.push_front(even);
-        start_pos -= 1;
-        auto get_pre_link = [&](node *v, int x) -> node * {
-            return start_pos + v->len + 1 < end_pos && get_data(start_pos + v->len + 1) == x ? v : v->dlink[x];
-        };
-        node *prepal = get_pre_link(v, x);
-        if (!prepal->next[x]) {
-            node *tmp = new_node(prepal->len + 2);
-            tmp->prev = prepal;
-            node *tmp_link = get_pre_link(prepal->link, x)->next[x];
-            if (!tmp_link) tmp_link = even;
-            tmp->set_link(tmp_link);
-            tmp->dlink = tmp->link->dlink;
-            tmp->dlink[get_data(start_pos + tmp->link->len)] = tmp->link;
-            prepal->next[x] = tmp;
+    void add_letter(char c, bool back = 1) {
+        back ? add_letter<1>(c) : add_letter<0>(c);
+    }
+    void pop_letter(bool back = 1) {
+        back ? pop_letter<1>() : pop_letter<0>();
+    }
+    int distinct() { return active; }
+
+    template<bool back = 1>
+    int maxlen() { return len[get<back>(states, 0)]; }
+
+    void pop_front() { pop_letter(0); }
+    void pop_back() { pop_letter(1); }
+    void push_front(char c) { add_letter(c, 0); }
+    void push_back(char c) { add_letter(c, 1); }
+    int longest_prefix_pal() { return maxlen<0>(); }
+    int longest_suffix_pal() { return maxlen<1>(); }
+
+    vector<array<int, 26>> to;
+    vector<int32_t> len, link, slink, par, cnt;
+
+    deque<char> s;
+    deque<int> states = {0}, diffs;
+    int sz = 2, active = 0;
+
+    template<bool back = 1>
+    int get_link(int v, char c) {
+        while(c != get<back>(s, len[v] + 1)) {
+            if(c == get<back>(s, len[link[v]] + 1)) v = link[v];
+            else v = slink[v];
         }
-        node *vp = prepal->next[x];
-        get_presurf(start_pos) = vp;
-        get_sufsurf(start_pos) = even;
-        get_sufsurf(start_pos + vp->len - 1) = vp;
-        if (vp->link->len >= 1 && get_presurf(start_pos + vp->len - vp->link->len) == vp->link)
-            get_presurf(start_pos + vp->len - vp->link->len) = even;
-        vp->cnt += 1;
+        return v;
     }
- 
-    void pop_back() {
-        int x = data.back();
-        node *v = get_sufpal();
-        if (unique(v)) {
-            v->prev->next[x] = nullptr;
-            del_node(v);
+
+    template<bool back = 1>
+    int make_to(int last, char c) {
+        last = get_link<back>(last, c);
+        if(!to[last][c]) {
+            int u = to[get_link<back>(link[last], c)][c];
+            link[sz] = u;
+            par[sz] = last;
+            len[sz] = len[last] + 2;
+
+            if(len[sz] - len[u] == len[u] - len[link[u]]) slink[sz] = slink[u];
+            else slink[sz] = u;
+            
+            to[last][c] = sz++;
         }
-        v->cnt -= 1;
-        if (v->link->len >= 1 && get_sufsurf(end_pos - v->len + v->link->len - 1)->len < v->link->len) {
-            get_sufsurf(end_pos - v->len + v->link->len - 1) = v->link;
-            get_presurf(end_pos - v->len) = v->link;
-        } else
-            get_presurf(end_pos - v->len) = even;
-        end_pos -= 1;
-        data.pop_back();
-        presurf.pop_back();
-        sufsurf.pop_back();
-    }
- 
-    void pop_front() {
-        int x = data.front();
-        node *v = get_prepal();
-        if (unique(v)) {
-            v->prev->next[x] = nullptr;
-            del_node(v);
-        }
-        v->cnt -= 1;
-        if (v->link->len >= 1 && get_presurf(start_pos + v->len - v->link->len)->len < v->link->len) {
-            get_presurf(start_pos + v->len - v->link->len) = v->link;
-            get_sufsurf(start_pos + v->len - 1) = v->link;
-        } else
-            get_sufsurf(start_pos + v->len - 1) = even;
-        start_pos += 1;
-        data.pop_front();
-        presurf.pop_front();
-        sufsurf.pop_front();
-    }
- 
-    int count_palindrome() {
-        return node_cnt - 2;
+        return to[last][c];
     }
 };
