24b082ac9b72fa0de671dcf3461484f9c6d39410
Abdullah Barakat <86885465+drynor@users.noreply.github.com>
2026-06-04 11:52:22 +0300

    Refactor eertree class for improved clarity and performance

--- String Algo/DequePlandromicTree.cpp
+++ String Algo/DequePlandromicTree.cpp
@@ -1,122 +1,130 @@
 struct eertree {
-    eertree(int q) {
-        q += 2;
-        cnt = len = par = link = slink = vector(q, 0);
-        to.resize(q);
+    vector<array<int, 26>> to;
+    vector<int> len, link, slink, par, cnt;
+    deque<int> s, states = {0}, diffs;
+    int sz = 2, active = 0;
+
+    eertree(int q) : to(q + 2), len(q + 2), link(q + 2),
+                     slink(q + 2), par(q + 2), cnt(q + 2) {
         link[0] = slink[0] = 1;
         len[1] = -1;
     }
 
-    template<bool back = 1>
-    static int get(auto const& d, int idx) {
-        if(idx >= size(d)) return -1;
-        else if constexpr (back) return prev(end(d))[-idx];
-        else return begin(d)[idx];
-    }
-    template<bool back = 1>
-    static void push(auto &d, auto c) {
-        if constexpr (back) d.push_back(c);
-        else d.push_front(c);
-    }
-    template<bool back = 1>
-    static void pop(auto &d) {
-        if constexpr (back) d.pop_back();
-        else d.pop_front();
+    template<class D>
+    static int get(const D& d, int i, bool back) {
+        if(i >= (int)d.size()) return -1;
+        return back ? d[(int)d.size() - 1 - i] : d[i];
     }
 
-    template<bool back = 1>
-    void add_letter(char c) {
-        c -= 'a';
-        push<back>(s, c);
-        int pre = get<back>(states, 0);
-        int last = make_to<back>(pre, c);
-        active += !(cnt[last]++);
-        int D = 2 + len[pre] - len[last];
-        while(D + len[pre] <= len[last]) {
-            pop<back>(states);
-            if(!empty(states)) {
-                pre = get<back>(states, 0);
-                D += get<back>(diffs, 0);
-                pop<back>(diffs);
-            }
-            else break;
-        }
-        if(!empty(states)) push<back>(diffs, D);
-        push<back>(states, last);
+    template<class D, class T>
+    static void push(D& d, T x, bool back) {
+        if(back) d.pb(x);
+        else d.pf(x);
     }
-    template<bool back = 1>
-    void pop_letter() {
-        int last = get<back>(states, 0);
-        active -= !(--cnt[last]);
-        pop<back>(states);
-        pop<back>(s);
-        array cands = {pair{link[last], len[last] - len[link[last]]},
-                       pair{par[last], 0}};
-        for(auto [state, diff]: cands) {
-            if(empty(states)) {
-                states = {state};
-                diffs = {diff};
-            } else {
-                int D = get<back>(diffs, 0) - diff;
-                int pre = get<back>(states, 0);
-                if(D + len[state] > len[pre]) {
-                    push<back>(states, state);
-                    pop<back>(diffs);
-                    push<back>(diffs, D);
-                    push<back>(diffs, diff);
-                }
-            }
-        }
-        pop<back>(diffs);
+
+    template<class D>
+    static void pop(D& d, bool back) {
+        if(back) d.popb();
+        else d.popf();
     }
-    void add_letter(char c, bool back = 1) {
-        back ? add_letter<1>(c) : add_letter<0>(c);
-    }
-    void pop_letter(bool back = 1) {
-        back ? pop_letter<1>() : pop_letter<0>();
-    }
-    int distinct() { return active; }
 
-    template<bool back = 1>
-    int maxlen() { return len[get<back>(states, 0)]; }
-
-    void pop_front() { pop_letter(0); }
-    void pop_back() { pop_letter(1); }
-    void push_front(char c) { add_letter(c, 0); }
-    void push_back(char c) { add_letter(c, 1); }
-    int longest_prefix_pal() { return maxlen<0>(); }
-    int longest_suffix_pal() { return maxlen<1>(); }
-
-    vector<array<int, 26>> to;
-    vector<int32_t> len, link, slink, par, cnt;
-
-    deque<char> s;
-    deque<int> states = {0}, diffs;
-    int sz = 2, active = 0;
-
-    template<bool back = 1>
-    int get_link(int v, char c) {
-        while(c != get<back>(s, len[v] + 1)) {
-            if(c == get<back>(s, len[link[v]] + 1)) v = link[v];
+    int get_link(int v, int c, bool back) {
+        while(c != get(s, len[v] + 1, back)) {
+            if(c == get(s, len[link[v]] + 1, back)) v = link[v];
             else v = slink[v];
         }
         return v;
     }
 
-    template<bool back = 1>
-    int make_to(int last, char c) {
-        last = get_link<back>(last, c);
+    int make_to(int last, int c, bool back) {
+        last = get_link(last, c, back);
+
         if(!to[last][c]) {
-            int u = to[get_link<back>(link[last], c)][c];
+            int u = to[get_link(link[last], c, back)][c];
+
             link[sz] = u;
             par[sz] = last;
             len[sz] = len[last] + 2;
 
-            if(len[sz] - len[u] == len[u] - len[link[u]]) slink[sz] = slink[u];
-            else slink[sz] = u;
-            
+            if(len[sz] - len[u] == len[u] - len[link[u]])
+                slink[sz] = slink[u];
+            else
+                slink[sz] = u;
+
             to[last][c] = sz++;
         }
+
         return to[last][c];
     }
+
+    void add(char ch, bool back = true) {
+        int c = ch - 'a';
+        push(s, c, back);
+
+        int pre = get(states, 0, back);
+        int last = make_to(pre, c, back);
+
+        active += !(cnt[last]++);
+
+        int D = 2 + len[pre] - len[last];
+
+        while(D + len[pre] <= len[last]) {
+            pop(states, back);
+
+            if(states.empty()) break;
+
+            pre = get(states, 0, back);
+            D += get(diffs, 0, back);
+            pop(diffs, back);
+        }
+
+        if(!states.empty()) push(diffs, D, back);
+        push(states, last, back);
+    }
+
+    void pl(bool back = true) {
+        int last = get(states, 0, back);
+
+        active -= !(--cnt[last]);
+
+        pop(states, back);
+        pop(s, back);
+
+        pair<int, int> cand[2] = {
+            {link[last], len[last] - len[link[last]]},
+            {par[last], 0}
+        };
+
+        for(auto [state, diff] : cand) {
+            if(states.empty()) {
+                states = {state};
+                diffs = {diff};
+            } else {
+                int D = get(diffs, 0, back) - diff;
+                int pre = get(states, 0, back);
+
+                if(D + len[state] > len[pre]) {
+                    push(states, state, back);
+                    pop(diffs, back);
+                    push(diffs, D, back);
+                    push(diffs, diff, back);
+                }
+            }
+        }
+
+        pop(diffs, back);
+    }
+
+    void pb(char c) { add(c, true); }
+    void pf(char c) { add(c, false); }
+
+    void popb() { pl(true); }
+    void popf() { pl(false); }
+
+    int distinct() const { return active; }
+
+    int maxlen(bool back = true) { return len[get(states, 0, back)]; }
+
+    int longest_suffix_pal() { return maxlen(true); }
+    int longest_prefix_pal() { return maxlen(false); }
 };
