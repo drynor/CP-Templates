a6bd80a70b6cbd912ac54d05e495228105a09b1e
Abdullah Barakat <86885465+drynor@users.noreply.github.com>
2026-06-09 15:09:57 +0300

    Refactor SuffixArray to SA and optimize methods

--- String Algo/SuffixArray.cpp
+++ String Algo/SuffixArray.cpp
@@ -1,75 +1,60 @@
-struct SuffixArray {
+struct SA {
     string s;
     int n;
-    vector<int> sa, rank, lcp, lg;
-    vector<vector<int>> st;
+    vector<int> sa, rank, lcp;
+    ST st;
 
-    SuffixArray(string& text) : s(text), n(s.length()){
-        bsa();
-        blcpa();
-        bst();
+    SA(string& text) : s(text), n(s.size()) {
+        bsa(); blcpa(); bst();
     }
 
     void bsa() {
-        string a = s;
-        a += "$";
-        int n2 = a.size();
+        string a = s + "$";
+        int m = a.size();
 
-        sa.resize(n2);
-        rank.resize(n2);
-        vector<int> new_rank(n2);
+        sa.resize(m);
+        rank.resize(m);
 
-        {
-            vector<int> c(256, 0);
-            for (int i = 0; i < n2; ++i) c[a[i]]++;
-            for (int i = 1; i < 256; ++i) c[i] += c[i - 1];
-            for (int i = n2 - 1; i >= 0; --i) sa[--c[a[i]]] = i;
+        vector<int> nw(m), p(m), c(max(256, m), 0);
 
-            rank[sa[0]] = 0;
-            for (int i = 1; i < n2; ++i) {
-                rank[sa[i]] = rank[sa[i - 1]];
-                if (a[sa[i]] != a[sa[i - 1]]) {
-                    rank[sa[i]]++;
-                }
-            }
-        }
+        for (char ch : a) c[ch]++;
+        for (int i = 1; i < 256; i++) c[i] += c[i - 1];
+        for (int i = m - 1; i >= 0; i--) sa[--c[a[i]]] = i;
 
-        for (int k = 1; k < n2; k <<= 1) {
-            vector<int> last(n2);
-            for (int i = 0; i < n2; ++i) last[i] = (sa[i] - k + n2) % n2;
+        for (int i = 1; i < m; i++)
+            rank[sa[i]] = rank[sa[i - 1]] + (a[sa[i]] != a[sa[i - 1]]);
 
-            vector<int> c(n2, 0);
-            for (int pos : last) c[rank[pos]]++;
-            for (int i = 1; i < n2; ++i) c[i] += c[i - 1];
-            for (int i = n2 - 1; i >= 0; --i) sa[--c[rank[last[i]]]] = last[i];
+        for (int k = 1; k < m; k <<= 1) {
+            for (int i = 0; i < m; i++) p[i] = (sa[i] - k + m) % m;
 
-            new_rank[sa[0]] = 0;
-            for (int i = 1; i < n2; ++i) {
-                new_rank[sa[i]] = new_rank[sa[i - 1]];
-                pair<int, int> prev = {rank[sa[i - 1]], rank[(sa[i - 1] + k) % n2]};
-                pair<int, int> curr = {rank[sa[i]], rank[(sa[i] + k) % n2]};
-                if (curr != prev) {
-                    new_rank[sa[i]]++;
-                }
-            }
-            rank = new_rank;
+            fill(c.begin(), c.begin() + m, 0);
+            for (int x : p) c[rank[x]]++;
+            for (int i = 1; i < m; i++) c[i] += c[i - 1];
+            for (int i = m - 1; i >= 0; i--) sa[--c[rank[p[i]]]] = p[i];
 
-            if (rank[sa[n2 - 1]] == n2 - 1) break;
+            auto key = [&](int i) {
+                return pair<int, int>{rank[i], rank[(i + k) % m]};
+            };
+
+            nw[sa[0]] = 0;
+            for (int i = 1; i < m; i++)
+                nw[sa[i]] = nw[sa[i - 1]] + (key(sa[i]) != key(sa[i - 1]));
+
+            rank.swap(nw);
+
+            if (rank[sa[m - 1]] == m - 1) break;
         }
 
         sa.erase(sa.begin());
 
         rank.resize(n);
-        for(int i = 0; i < n; ++i) {
-            rank[sa[i]] = i;
-        }
+        for (int i = 0; i < n; i++) rank[sa[i]] = i;
     }
 
     void blcpa() {
-        if (n <= 1) return;
-        lcp.resize(n - 1);
-        int h = 0;
-        for (int i = 0; i < n; i++) {
+        lcp.assign(n - 1, 0);
+
+        for (int i = 0, h = 0; i < n; i++) {
             if (rank[i] == n - 1) {
                 h = 0;
                 continue;
@@ -80,49 +65,24 @@ struct SuffixArray {
             while (i + h < n && j + h < n && s[i + h] == s[j + h]) h++;
 
             lcp[rank[i]] = h;
-
-            if(h > 0) h--;
+            if (h) h--;
         }
     }
 
     void bst() {
-        if (n <= 1) return;
-        int len = n - 1;
-        lg.resize(len + 1);
-        lg[1] = 0;
-        for (int i = 2; i <= len; i++) {
-            lg[i] = lg[i / 2] + 1;
-        }
-
-        int mx = lg.empty() ? -1 : lg.back();
-        if (mx < 0) return;
-
-        st.assign(mx + 1, vector<int>(len));
-        
-        if (!lcp.empty()) {
-            st[0] = lcp;
-        }
-
-        for (int k = 1; k <= mx; k++) {
-            for (int i = 0; i + (1 << k) <= len; i++) {
-                st[k][i] = min(st[k - 1][i], st[k - 1][i + (1 << (k - 1))]);
-            }
-        }
+        if (!lcp.empty()) st.build(lcp);
     }
 
-    int query(int l, int r)  {
-        if (l > r) return n;
-        int k = lg[r - l + 1];
-        return min(st[k][l], st[k][r - (1 << k) + 1]);
+    int query(int l, int r) {
+        return l > r ? n : st.query(l, r);
     }
 
-    int getLCP(int i, int j)  {
+    int getLCP(int i, int j) {
         if (i == j) return n - i;
 
-        int ri = rank[i];
-        int rj = rank[j];
-
+        int ri = rank[i], rj = rank[j];
         if (ri > rj) swap(ri, rj);
+
         return query(ri, rj - 1);
     }
 };
