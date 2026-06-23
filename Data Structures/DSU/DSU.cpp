fb90a6f208408a97568d10f2b523c79c60f7c251
Abdullah Barakat <86885465+drynor@users.noreply.github.com>
2026-06-23 16:26:18 +0300

    Remove inline specifier from get and unite methods

--- Data Structures/DSU/DSU.cpp
+++ Data Structures/DSU/DSU.cpp
@@ -6,17 +6,15 @@ struct dsu {
         sz.resize(n, 1);
         iota(p.begin(), p.end(), 0);
     }
-    inline int get(int x) {
+    int get(int x) {
         return (x == p[x] ? x : (p[x] = get(p[x])));
     }
-    inline bool unite(int x, int y) {
+    bool unite(int x, int y) {
         x = get(x); y = get(y);
         if (x != y) {
             if (sz[x] < sz[y]) swap(x, y); 
             p[y] = x;
             sz[x] += sz[y];
-            return true;
         }
-        return false;
     }
 };
