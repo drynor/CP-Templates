d1756e7ec920e9103c005a8584efc4498b1a267f
Abdullah Barakat <86885465+drynor@users.noreply.github.com>
2026-09-11 19:36:36 +0300

    Update PURQ.cpp

--- Data Structures/BIT/PURQ.cpp
+++ Data Structures/BIT/PURQ.cpp
@@ -1,19 +1,19 @@
-template <class T> struct BIT {
+struct BIT {
     int n;
-    vector<T> bit;
+    vector<int> bit;
     BIT(int sz) {
         n = sz + 1;
         bit.assign(n, 0);
     }
-    T qry(int r) {
-        T v = 0;
+    int qry(int r) {
+        int v = 0;
         for (int i = r + 1; i > 0; i -= (i & -i)) v += bit[i];
         return v;
     }
-    T qry(int l, int r) {
+    int qry(int l, int r) {
         return qry(r) - qry(l - 1);
     }
-    void upd(int j, T v) {
+    void upd(int j, int v) {
         for (int i = j + 1; i < n; i += (i & -i)) bit[i] += v;
     }
 };
