51ec70844b459d65777cb88283c1f710127ca1ca
Abdullah Barakat <86885465+drynor@users.noreply.github.com>
2026-09-11 19:36:50 +0300

    Refactor BIT to use int instead of template type

--- Data Structures/BIT/RUPQ.cpp
+++ Data Structures/BIT/RUPQ.cpp
@@ -1,19 +1,19 @@
-template <class T> struct BIT {
+struct BIT {
     int n;
-    vector<T> bit;
+    vector<int> bit;
     BIT(int sz) {
         n = sz + 1;
         bit.assign(n, 0);
     }
-    T qry(int j) {
-        T v = 0;
+    int qry(int j) {
+        int v = 0;
         for (int i = j + 1; i > 0; i -= (i & -i)) v += bit[i];
         return v;
     }
-    void upd(int j, T v) {
+    void upd(int j, int v) {
         for (int i = j + 1; i < n; i += (i & -i)) bit[i] += v;
     }
-    void upd(int l, int r, T v) {
+    void upd(int l, int r, int v) {
         if (l > r) return;
         upd(l, v);
         upd(r + 1, -v);
