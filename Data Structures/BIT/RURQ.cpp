e8de55f424c7a51063801c2f3e358cf1fb4830e3
Abdullah Barakat <86885465+drynor@users.noreply.github.com>
2026-09-11 19:37:00 +0300

    Refactor BIT structure to use int instead of template

--- Data Structures/BIT/RURQ.cpp
+++ Data Structures/BIT/RURQ.cpp
@@ -1,32 +1,32 @@
-template <class T> struct BIT {
+struct BIT {
     int n;
-    vector<T> bit1, bit2;
+    vector<int> bit1, bit2;
 
     BIT(int sz) {
         n = sz + 1;
         bit1.assign(n, 0);
         bit2.assign(n, 0);
     }
-    void upd(vector<T>& b, int idx, T val) {
+    void upd(vector<int>& b, int idx, int val) {
         for (; idx < n; idx += idx & -idx)
             b[idx] += val;
     }
-    T qry(vector<T>& b, int idx) {
+    int qry(vector<int>& b, int idx) {
         T v = 0;
         for (; idx > 0; idx -= idx & -idx)
             v += b[idx];
         return v;
     }
-    void upd(int l, int r, T val) {
+    void upd(int l, int r, int val) {
         upd(bit1, l + 1, val);
         upd(bit2, l + 1, val * l);
         upd(bit1, r + 2, -val);
         upd(bit2, r + 2, -val * (r + 1));
     }
-    T pqry(int idx) {
+    int pqry(int idx) {
         return qry(bit1, idx + 1) * (idx + 1) - qry(bit2, idx + 1);
     }
-    T qry(int l, int r) {
+    int qry(int l, int r) {
         return pqry(r) - pqry(l - 1);
     }
 };
