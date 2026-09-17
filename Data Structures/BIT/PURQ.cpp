05bc1ca33ea40716520925d534d20d60cf71f778
Abdullah Barakat <86885465+drynor@users.noreply.github.com>
2026-09-17 19:19:42 +0300

    Update PURQ.cpp

--- Data Structures/BIT/PURQ.cpp
+++ Data Structures/BIT/PURQ.cpp
@@ -16,4 +16,11 @@ struct BIT {
     void upd(int j, int v) {
         for (int i = j + 1; i < n; i += (i & -i)) bit[i] += v;
     }
+    // smallest i (0-indexed) with prefix sum qry(i) >= k; returns sz if none. needs all values >= 0
+    int kth(int k) {
+        int pos = 0;
+        for (int pw = 1 << __lg(n); pw; pw >>= 1)
+            if (pos + pw < n && bit[pos + pw] < k) pos += pw, k -= bit[pos];
+        return pos;
+    }
 };
