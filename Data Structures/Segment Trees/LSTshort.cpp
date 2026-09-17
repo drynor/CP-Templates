bda843a6c7b75fbbab53cfebad023881accdc3da
Abdullah Barakat <86885465+drynor@users.noreply.github.com>
2026-09-17 19:39:02 +0300

    Refactor segment tree implementation for clarity

--- Data Structures/Segment Trees/LSTshort.cpp
+++ Data Structures/Segment Trees/LSTshort.cpp
@@ -1,24 +1,28 @@
+// lazy range add, range sum. 2n-1 nodes: root 0, left = i + 1, right = i + 2 * (m - s + 1). 0-based [l, r]
 struct seg {
     vector<int> t, us;
-    vector<bool> lz;
     int n, def = 0;
     seg(int tn) {
         n = tn;
-        t.resize(n * 4, def);
-        lz.resize(n * 4, false);
-        us.resize(n * 4, 0);
+        t.assign(2 * n, def);
+        us.assign(2 * n, 0);
+    }
+    void build(int s, int e, int i, vector<int>& a) { // reference only: build(0, n - 1, 0, a)
+        if (s == e) { t[i] = a[s]; return; }
+        int m = (s + e) / 2, l = i + 1, r = i + 2 * (m - s + 1);
+        build(s, m, l, a); build(m + 1, e, r, a);
+        t[i] = t[l] + t[r];
     }
     void apply(int i, int s, int e, int u){
-        lz[i] = true;
         us[i] += u;
         t[i] += u * (e - s + 1);
     }
     void push(int i, int s, int e){
-        if(lz[i]){
+        if(us[i]){
             int m = (s + e) / 2;
-            apply(2 * i, s, m, us[i]); 
-            apply(2 * i + 1, m + 1, e, us[i]);
-            us[i] = lz[i] = 0;
+            apply(i + 1, s, m, us[i]);
+            apply(i + 2 * (m - s + 1), m + 1, e, us[i]);
+            us[i] = 0;
         }
     }
     void upd(int s, int e, int i, int l, int r, int u) {
@@ -27,16 +31,18 @@ struct seg {
             apply(i, s, e, u); return;
         }
         push(i, s, e);
-        int m = (s + e) / 2;
-        upd(s, m, 2 * i, l, r, u);
-        upd(m + 1, e, 2 * i + 1, l, r, u);
-        t[i] = t[2 * i] + t[2 * i + 1];
+        int m = (s + e) / 2, lc = i + 1, rc = i + 2 * (m - s + 1);
+        upd(s, m, lc, l, r, u);
+        upd(m + 1, e, rc, l, r, u);
+        t[i] = t[lc] + t[rc];
     }
     int qry(int s, int e, int i, int l, int r) {
         if (s > r || e < l)   return def;
         if (s >= l && e <= r) return t[i];
         push(i, s, e);
         int m = (s + e) / 2;
-        return qry(s, m, 2 * i, l, r) + qry(m + 1, e, 2 * i + 1, l, r);
+        return qry(s, m, i + 1, l, r) + qry(m + 1, e, i + 2 * (m - s + 1), l, r);
     }
+    void upd(int l, int r, int u) { upd(0, n - 1, 0, l, r, u); }
+    int qry(int l, int r) { return qry(0, n - 1, 0, l, r); }
 };
