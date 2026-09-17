2b4e6775c4bd5f4fb1ea5bbae2dbc48d88c9e688
Abdullah Barakat <86885465+drynor@users.noreply.github.com>
2026-09-17 19:43:34 +0300

    Refactor lazy segment tree implementation

--- Data Structures/Segment Trees/LSTlong.cpp
+++ Data Structures/Segment Trees/LSTlong.cpp
@@ -1,14 +1,21 @@
+// generic lazy segment tree. 2n-1 nodes: root 0, left = i + 1, right = i + 2 * (m - s + 1). 0-based [l, r]
 template<typename node, typename update>
 struct seg {
     vector<node> t;
-    vector<bool> lazy;
+    vector<char> lazy;
     vector<update> us;
     int n;
     seg(int tn) {
         n = tn;
-        t.resize(n * 4);
-        lazy.resize(n * 4, false);
-        us.resize(n * 4);
+        t.resize(2 * n);
+        lazy.assign(2 * n, 0);
+        us.resize(2 * n);
+    }
+    void build(int s, int e, int i, vector<node>& a) { // reference only: build(0, n - 1, 0, a)
+        if (s == e) { t[i] = a[s]; return; }
+        int m = (s + e) / 2, l = i + 1, r = i + 2 * (m - s + 1);
+        build(s, m, l, a); build(m + 1, e, r, a);
+        t[i].merge(t[l], t[r]);
     }
     void apply(int i, int s, int e, update& u){
         if(s != e){
@@ -20,8 +27,8 @@ struct seg {
     void push(int i, int s, int e){
         if(lazy[i]){
             int m = (s + e) / 2;
-            apply(2 * i, s, m, us[i]); 
-            apply(2 * i + 1, m + 1, e, us[i]);
+            apply(i + 1, s, m, us[i]);
+            apply(i + 2 * (m - s + 1), m + 1, e, us[i]);
             us[i] = update();
             lazy[i] = 0;
         }
@@ -32,10 +39,10 @@ struct seg {
             apply(i, s, e, u); return;
         }
         push(i, s, e);
-        int m = (s + e) / 2;
-        upd(s, m, 2 * i, l, r, u);
-        upd(m + 1, e, 2 * i + 1, l, r, u);
-        t[i].merge(t[2 * i], t[2 * i + 1]);
+        int m = (s + e) / 2, lc = i + 1, rc = i + 2 * (m - s + 1);
+        upd(s, m, lc, l, r, u);
+        upd(m + 1, e, rc, l, r, u);
+        t[i].merge(t[lc], t[rc]);
     }
     node qry(int s, int e, int i, int l, int r) {
         if (s > r || e < l)   return node();
@@ -43,18 +50,14 @@ struct seg {
         push(i, s, e);
         int m = (s + e) / 2;
         node ln, rn, ans;
-        ln = qry(s, m, 2 * i, l, r);
-        rn = qry(m + 1, e, 2 * i + 1, l, r);
+        ln = qry(s, m, i + 1, l, r);
+        rn = qry(m + 1, e, i + 2 * (m - s + 1), l, r);
         ans.merge(ln, rn);
         return ans;
     }
-    void upd(int l, int r, int val) {
-        update nw = update(val);
-        upd(0, n - 1, 1, l, r, nw);
-    }
-    node qry(int l, int r) {
-        return qry(0, n - 1, 1, l, r);
-    }
+    void upd(int l, int r, int val) { update nw = update(val); upd(0, n - 1, 0, l, r, nw); }
+    void upd(int l, int r, update u) { upd(0, n - 1, 0, l, r, u); }
+    node qry(int l, int r) { return qry(0, n - 1, 0, l, r); }
 };
 struct node1 {
     int sum = 0;
